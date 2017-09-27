1627827046b248c2f6e336f9b5e5131ba1b9fbd6
rxtan2 <rxtan2@illinois.edu>
2017-09-26 21:43:24 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,7 +13,3 @@ fi
 
 mvn clean compile
 mvn exec:java -Dexec.mainClass="edu.illinois.cs.cogcomp.wikiparser.wikiparse.Runner" -Dexec.args="$1 $4 $5"
-
-# sh run.sh C:\\Users\\Reuben-PC\\Desktop\\wikiparserfiles 2 C:\\Users\\Reuben-PC\\Desktop\\dewiki-20170220-stub-articles1.xml.gz C:\\Users\\Reuben-PC\\Desktop\\text C:\\tmp false
-# ./run.sh C:\\Users\\Reuben-PC\\Desktop\\text C:\\tmp
-# sh run.sh 2 C:\\Users\\Reuben-PC\\Desktop\\enwiki-20170520-pages-articles.xml.bz2 C:\\Users\\Reuben-PC\\Desktop\\test C:\\tmp
