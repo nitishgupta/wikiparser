ef94c166a12c4cf0cc756afd990412cc9d1898ff
rxtan2 <rxtan2@illinois.edu>
2017-09-28 10:34:57 -0500

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-# This shell script takes 4 arguments.
-# First argument: Number of processors to be used
-# Second argument: File path of the wiki dump to be parsed
-# Third argument: File path of your desired location for your output for the wikiextractor.py
-# Fourth argument: File path of desired output for the java wiki parser
-
-python wikiextractor.py --filter_disambig_pages --processes=$1 --links -o $3 $2 
-mvn clean compile
-mvn exec:java -Dexec.mainClass="edu.illinois.cs.cogcomp.wikiparser.wikiparse.WikiExtractParser" -Dexec.args="$3 $4"
