995e20da06f9acc14b763ed7b53f8999b0aa3171
rxtan2 <rxtan2@illinois.edu>
2017-10-30 21:36:28 -0500

    Remove scripts from main project folder

--- runjwplparser.sh
+++
@@ -1,5 +0,0 @@
-# This shell script takes 1 argument.
-# First argument: File path to directory for the output files of the JWPL parser
-
-mvn clean compile -e
-mvn exec:java -Dexec.mainClass="edu.illinois.cs.cogcomp.wikiparser.jwpl.JWPLParser" -Dexec.args="$1"
