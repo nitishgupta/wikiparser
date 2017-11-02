795d21e692c24602826b7e0cb4893a943eeea110
rxtan2 <rxtan2@illinois.edu>
2017-11-02 10:46:01 -0500

    Pushed new script

--- scripts/runjwplparser.sh
+++ scripts/runjwplparser.sh
@@ -1,2 +1,2 @@
 mvn clean compile -e
-mvn exec:java -Dexec.mainClass="edu.illinois.cs.cogcomp.wikiparser.jwpl.JWPLParser"
+mvn exec:java -Dexec.mainClass="edu.illinois.cs.cogcomp.wikiparser.jwpl.JWPLParser" -Dexec.args="$1"
