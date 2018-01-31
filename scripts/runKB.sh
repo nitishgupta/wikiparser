faaba7657f0fdecd7794d7822b1c1a9bfd770972
Reuben Tan <rxtan2@bronte.cs.illinois.edu>
2018-01-31 17:00:31 -0600

    Added instructions in runKB.sh

--- scripts/runKB.sh
+++ scripts/runKB.sh
@@ -1,2 +1,8 @@
+# This shell script takes 4 arguments.
+# First argument: File path of curIds2Title.tsv
+# Second argument: File path of resCurIdNonDisambig2ResTitle_nonList.tsv
+# Third argument: File path of resCurId2Redirects.tsv
+# Fourth argument: File path for output RedirectTitle2ResolvedTitleMap
+
 mvn clean compile
-mvn exec:java -Dexec.mainClass="edu.illinois.cs.cogcomp.wikiparser.unifiedParsing.KB" -Dexec.args="$1 $2 $3"
+mvn exec:java -Dexec.mainClass="edu.illinois.cs.cogcomp.wikiparser.unifiedParsing.KB" -Dexec.args="$1 $2 $3 $4"
--- src/main/java/edu/illinois/cs/cogcomp/wikiparser/unifiedParsing/KB.java
+++ src/main/java/edu/illinois/cs/cogcomp/wikiparser/unifiedParsing/KB.java
@@ -5,11 +5,34 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 public class KB {
     public static Map<String, String> curIds2TitleMap = null;
     public static Map<String, String> nonListMap = null;
     public static Map<String, String> RedirectTitle2ResolvedTitleMap = null;
+    private static final String outputFile = "RedirectTitle2ResolvedTitle.tsv";
+
+    public static void writeToFile(String outputDir){
+      Path filePath = Paths.get(outputDir, outputFile);
+      File file = new File(filePath.toString());
+      try{
+          FileWriter fw = new FileWriter(file.getAbsoluteFile());
+          BufferedWriter bw = new BufferedWriter(fw);
+          for(String redirectTitle : RedirectTitle2ResolvedTitleMap.keySet()){
+              String resTitle = RedirectTitle2ResolvedTitleMap.get(redirectTitle);
+              bw.write(redirectTitle + "\t" + resTitle + "\n"); // Each page exists on a new line
+          }
+          bw.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
 
     public static void loadRedirectTitle2ResolvedTitleMap(String filename){
         // Parses resCurId2Redirects.tsv
@@ -79,5 +102,6 @@ public class KB {
       System.out.println("Size of nonListMap is " + nonListMap.size());
       loadRedirectTitle2ResolvedTitleMap(args[2]);
       System.out.println("Size of RedirectTitle2ResolvedTitleMap is " + RedirectTitle2ResolvedTitleMap.size());
+      writeToFile(args[3]);
     }
 }
